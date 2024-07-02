f4a2fd8186fef2e7518d791c58abd19e6f4b0aec
Sarnuraaai <149012432+Sarnuraaai@users.noreply.github.com>
2024-07-03 00:45:22 +0300

    Paint released

---
+++ oop/paint/Circle.java
@@ -0,0 +1,30 @@
+package oop.paint;
+
+public class Circle extends Figure implements Drawable {
+    private double radius;
+
+    public Circle(Point point, double radius) {
+        super(point);
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+
+    @Override
+    public String draw() {
+        return "чёрный круг с координатами (x: " + this.getPoint().x + ", y: " + this.getPoint().y + ")";
+    }
+
+    @Override
+    public String draw(Color color) {
+        return color.getColor() + " круг с координатами (x: " + this.getPoint().x + ", y: " + this.getPoint().y + ")";
+    }
+}
