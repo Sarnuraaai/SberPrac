a50e4b0b6a005d3085bfb0aa71f809b179f7d8ab
Sarnuraaai <149012432+Sarnuraaai@users.noreply.github.com>
2024-07-10 18:56:20 +0300

    fix

--- MavenProject/streams/src/main/java/fifth/StringToBoolean.java
+++ MavenProject/streams/src/main/java/fifth/StringToBoolean.java
@@ -3,14 +3,11 @@ package fifth;
 import java.util.stream.Stream;
 
 public class StringToBoolean {
-    public static void main(String[] args) {
-        String string = "1010010111";
+    public boolean stringToBoolean(String string) {
         Stream<Boolean> stream = string.codePoints()
                 .mapToObj(ch -> String.valueOf((char) ch).equals("1"));
 
-        boolean result = stream.reduce(true, (ch1, ch2) -> ch1 && ch2);
-
-        System.out.println(result);
+        return stream.reduce(true, (ch1, ch2) -> ch1 && ch2);
     }
 
 
